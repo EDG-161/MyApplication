2bc1a0686b87b2d9d9d80f9298de20d0f669947f
EDG-161 <44904881+EDG-161@users.noreply.github.com>
2020-01-24 08:17:54 -0600

    Initial commit

---
+++ app/src/main/java/com/main/myapplication/IsoDepTransceiver.java
@@ -0,0 +1,89 @@
+package com.main.myapplication;
+
+import android.nfc.tech.IsoDep;
+
+import java.io.IOException;
+
+public class IsoDepTransceiver implements Runnable {
+
+
+    public interface OnMessageReceived {
+
+        void onMessage(byte[] message);
+
+        void onError(Exception exception);
+
+        void clearmessages();
+    }
+
+    private IsoDep isoDep;
+    private OnMessageReceived onMessageReceived;
+    private byte[] select_apdu;
+    private byte[] readrecord_apdu;
+
+    public IsoDepTransceiver(IsoDep isoDep, OnMessageReceived onMessageReceived) {
+        this.isoDep = isoDep;
+        this.onMessageReceived = onMessageReceived;
+        this.select_apdu = new byte[] { (byte) 0x94, (byte)0xA4, 0x00, 0x00, (byte) 0x02, (byte)0x20, 0x69, 0x00 };
+        this.readrecord_apdu = new byte[] { (byte) 0x94, (byte)0xB2, 0x01, 0x04, 0x00 };
+    }
+
+    @Override
+    public void run() {
+
+        boolean errorfound = false;
+
+        onMessageReceived.clearmessages();
+        try{
+            System.out.println("Leyendo");
+            isoDep.close();
+            isoDep.connect();
+        }
+        catch(IOException e)
+        {
+            errorfound = true;
+            e.printStackTrace();
+            onMessageReceived.onMessage("Unable to connect to card, please try again!".getBytes());
+        }
+        if(!errorfound) {
+            try {
+                byte[] response = new byte[0];
+                isoDep.transceive(select_apdu);
+                System.out.println("Leyendo2");
+                response = isoDep.transceive(readrecord_apdu);
+                if (response != null) {
+                    byte contractcounters;
+                    for (int i = 0; (i*3)+3 < response.length -1; i++) {
+                        contractcounters = (byte) (response[(i * 3)] + response[(i * 3) + 1] + response[(i * 3) + 2]);
+                        if (contractcounters > 0) {
+                            byte[] message = ("Ticket " + (i + 1) + ": ").getBytes();
+                            byte[] ridesleftmsg = " rides left.".getBytes();
+                            byte[] logmessage = new byte[message.length + ridesleftmsg.length + 2];
+                            System.arraycopy(message, 0, logmessage, 0, message.length);
+                            byte[] currentcounter = new byte[1];
+                            currentcounter[0] = contractcounters;
+                            byte[] currentcounterbytes = bytesToHex(currentcounter).getBytes();
+                            logmessage[message.length] = currentcounterbytes[0];
+                            logmessage[message.length + 1] = currentcounterbytes[1];
+                            System.arraycopy(ridesleftmsg, 0, logmessage, message.length + 2, ridesleftmsg.length);
+                            onMessageReceived.onMessage(logmessage);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                onMessageReceived.onError(e);
+            }
+        }
+    }
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+}
---
+++ app/src/main/java/com/main/myapplication/MainActivity.java
@@ -0,0 +1,148 @@
+package com.main.myapplication;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcAdapter.ReaderCallback;
+import android.nfc.Tag;
+import android.nfc.tech.IsoDep;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+
+import com.main.myapplication.IsoDepTransceiver.OnMessageReceived;
+
+public class MainActivity extends Activity implements OnMessageReceived, ReaderCallback {
+
+    private NfcAdapter nfcAdapter;
+    private ListView listView;
+    private IsoDepAdapter isoDepAdapter;
+    private IsoDepTransceiver isoDepTransceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.listView);
+        isoDepAdapter = new IsoDepAdapter(getLayoutInflater());
+        listView.setAdapter(isoDepAdapter);
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        TabHost host = (TabHost)findViewById(R.id.tabHost);
+        host.setup();
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Scan");
+        spec.setContent(R.id.linearLayoutScan);
+        spec.setIndicator("Scan");
+        host.addTab(spec);
+
+        //Tab 2
+        spec = host.newTabSpec("Help");
+        spec.setContent(R.id.linearLayoutHelp);
+        spec.setIndicator("Help");
+        host.addTab(spec);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (nfcAdapter == null) {
+            nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+            try {
+                wait(500);
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+        nfcAdapter.enableReaderMode(this, this, NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK | NfcAdapter.FLAG_READER_NFC_B,
+                null);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        nfcAdapter.disableReaderMode(this);
+        nfcAdapter = null;
+    }
+
+    @Override
+    public void onTagDiscovered(Tag tag) {
+        IsoDep isoDep = IsoDep.get(tag);
+
+        this.isoDepTransceiver = new IsoDepTransceiver(isoDep, this);
+        Thread thread = new Thread(this.isoDepTransceiver);
+        thread.start();
+        System.out.println("Detectado");
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        v.vibrate(100);
+    }
+
+    @Override
+    public void onMessage(final byte[] message) {
+        runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                //isoDepAdapter.addMessage(bytesToHex(message));
+                isoDepAdapter.addMessage(new String(message).replace("0A","10"));
+            }
+        });
+    }
+
+    @Override
+    public void clearmessages() {
+        runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                //isoDepAdapter.addMessage(bytesToHex(message));
+                isoDepAdapter.clearMessages();
+            }
+        });
+    }
+
+
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    @Override
+    public void onError(Exception exception) {
+        String errormsg = exception.getMessage();
+        if (errormsg != null) {
+            onMessage(errormsg.getBytes());
+        } else {
+            onMessage("Communication error, please try again".getBytes());
+        }
+
+        this.isoDepTransceiver = null;
+        nfcAdapter.disableReaderMode(this);
+        nfcAdapter = null;
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        nfcAdapter.enableReaderMode(this, this, NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK | NfcAdapter.FLAG_READER_NFC_B,
+                null);
+    }
+}
